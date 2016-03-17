f612671652e50491b14333d684c637e2eb80f66e
Bryant Jackson <bjackson_seller@outlook.com>
2016-03-16 20:32:24 -0400

    test paypal

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,6 +4,6 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@base', dirname(dirname(__DIR__)));
-Yii::setAlias('@url', "tenbillionbottles.com");
+Yii::setAlias('@url', "http://tenbillionbottles.com");
 
 require Yii::getAlias('@base') . '/vendor/autoload.php';
